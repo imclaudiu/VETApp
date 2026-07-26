f34adcd6833df8900153ab577bf001bc0093dc72
imclaudiu <imclaudiu@yahoo.com>
2026-07-26 15:29:33 +0300

    Kafka integrated with docker

--- BE/auth/src/main/java/com/vetapp/config/KafkaProducerConfig.java
+++ BE/auth/src/main/java/com/vetapp/config/KafkaProducerConfig.java
@@ -19,8 +19,8 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, Authentication> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps); //might bug
--- BE/user/src/main/java/com/vetapp/config/KafkaConsumerConfig.java
+++ BE/user/src/main/java/com/vetapp/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.vetapp.config;
 
 import com.vetapp.entity.Authentication;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +23,8 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, Authentication> consumerFactory() {
 
         Map<String, Object> configProps = new HashMap<>();
-
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+//        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "user-service-group");
         configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
